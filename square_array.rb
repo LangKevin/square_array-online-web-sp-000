fdea66636ed8958bafa9576a422006301a759a44
kevin lang <langkevina@gmail.com>
2019-05-19 23:24:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   myArr = new array
   array.each do |item|
-    my_array.unshift(item * 2)     
+    my_array.unshift(item * 2)
   end
   return myArr
 end

0636cb6f42296fbde89a4e3876be8ca3fc0adde3
kevin lang <langkevina@gmail.com>
2019-05-19 23:19:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   myArr = array
   myArr.each do |item|
-    myArr[item\ = item + 2
+    myArr[item] = item + 2
     puts "Whoooo are you?"
   end
   return myArr
